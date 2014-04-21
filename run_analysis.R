e2670070b6f24c8b4f3492c37a9a8f3f227fc380
Eduardo Blancas Reyes <edu@cacaolabs.co>
2014-04-20 19:34:03 -0500

    Initial commit

---
+++ run_analysis.R
@@ -0,0 +1,54 @@
+#Load column names
+column_names<-read.table("features.txt")
+#Filter columns (only mean and std measurements)
+column_indexes<-grep("mean|std", column_names[,2])
+
+#Load activity labels (for y datasets)
+activity_labels<-read.table("activity_labels.txt")
+
+#Load files (subject)
+subject_test<-read.table("subject_test.txt")
+subject_train<-read.table("subject_train.txt")
+#Set labels for subject datasets
+names(subject_test)<-c("subjectnumber")
+names(subject_train)<-c("subjectnumber")
+
+#Load files (x)
+x_test<-read.table("X_test.txt")
+x_train<-read.table("X_train.txt")
+#Get only columns with mean and std
+x_test<-x_test[,column_indexes]
+x_train<-x_train[,column_indexes]
+#Set labels for x datasets
+names(x_test)<-column_names[column_indexes,2]
+names(x_train)<-column_names[column_indexes,2]
+
+#Load files (y)
+y_test<-read.table("y_test.txt")
+y_train<-read.table("y_train.txt")
+#Merge y datasets (train and test) with activity labels
+y_test<-merge(activity_labels, y_test)
+y_train<-merge(activity_labels, y_train)
+#Set labels for y datasets
+names(y_test)<-c("activitycode", "activitydescription")
+names(y_train)<-c("activitycode", "activitydescription")
+
+#Create data frame from test data
+test_df<-data.frame(subject_test, x_test, y_test)
+#Create data frame from train data
+train_df<-data.frame(subject_train, x_train, y_train)
+
+#Combine test and train data frame
+dataset<-rbind(test_df, train_df)
+
+#Split dataset by subject and acvitity
+splitdatasets<-split(dataset, list(dataset$subjectnumber, dataset$activitycode), drop=TRUE)
+
+#Calculate column means (only to certain columns) and transpose
+colmeans<-data.frame(t(sapply(splitdatasets, function(x) colMeans(x[, 2:80], na.rm=TRUE))))
+#Get row.names
+subject.activity<-row.names(colmeans)
+#Delete row.names column
+row.names(colmeans)<-NULL
+#Create new subject.activity column
+colmeans<-cbind(subject.activity, colmeans)
